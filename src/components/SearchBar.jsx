1860daeda08081b9e9c8c1660d846a0247391e26
tomhuhges <merzbear@gmail.com>
2016-12-17 08:50:02 +0000

    move handleClick to container component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import { API_KEY } from '../apikeys';
 
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    const searchTerm = this.search.value;
-    const url = `https://api.flickr.com/services/rest/?api_key=${API_KEY}&method=flickr.photos.search&format=json&nojsoncallback=1&&per_page=25&page=1&text=${searchTerm}`;
-    fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data.photos.photo);
-      })
-      .catch((error) => {
-        throw error;
-      });
-  }
   render() {
     return (
       <div className="searchbar">
@@ -29,11 +12,15 @@ class SearchBar extends React.Component {
         />
         <button
           className="btn"
-          onClick={this.handleClick}
+          onClick={this.props.handleClick}
         >Go</button>
       </div>
     );
   }
 }
+
+SearchBar.propTypes = {
+  handleClick: React.PropTypes.func.isRequired,
+};
 
 export default SearchBar;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,36 @@ import React from 'react';
 import { render } from 'react-dom';
 import SearchBar from './components/SearchBar';
 import Images from './components/Images';
+import { API_KEY } from './apikeys';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    const searchTerm = this.searchBar.search.value;
+    const url = `https://api.flickr.com/services/rest/?api_key=${API_KEY}&method=flickr.photos.search&format=json&nojsoncallback=1&&per_page=25&page=1&text=${searchTerm}`;
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({ ...data.photos.photo });
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
   render() {
     return (
       <div className="container">
-        <SearchBar />
-        <Images />
+        <SearchBar
+          ref={(c) => { this.searchBar = c; }}
+          handleClick={this.handleClick}
+        />
+        <Images images={this.state.images} />
       </div>
     );
   }
