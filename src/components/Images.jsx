9575bcd1ab5bf46e00c59966ef7e13d77a85a1a5
tomhuhges <merzbear@gmail.com>
2016-12-17 07:07:59 +0000

    add Search and Images components

---
+++ src/components/Images.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Images extends React.Component {
+  render() {
+    return (
+      <div className="images">
+      </div>
+    );
+  }
+}
+
+export default Images;
