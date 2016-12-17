5173b0a04f36671d60ba0e68dd45b2682a7b6fed
tomhuhges <merzbear@gmail.com>
2016-12-17 06:14:29 +0000

    init

---
+++ webpack.config.babel.js
@@ -0,0 +1,73 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import merge from 'webpack-merge';
+import validate from 'webpack-validator';
+import dev from './webpack.config.dev';
+import prod from './webpack.config.prod';
+
+const paths = {
+  src: path.join(__dirname, '/src'),
+  dist: path.join(__dirname, '/dist'),
+  css: path.join(__dirname, '/src/css'),
+  images: path.join(__dirname, '/src/assets/img'),
+};
+
+const defaults = {
+  entry: {
+    app: paths.src,
+    style: `${paths.css}/style.css`,
+  },
+  output: {
+    path: paths.dist,
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        include: paths.src,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'vidsy flickr viewer',
+      template: './src/index.ejs',
+    }),
+  ],
+};
+
+let config;
+
+if (process.env.npm_lifecycle_event === 'build') {
+  // prod config
+  config = merge(
+    defaults,
+    prod({ paths }),
+  );
+} else {
+  // dev config
+  config = merge(
+    defaults,
+    dev({
+      paths,
+      server: {
+        host: process.env.HOST,
+        port: process.env.PORT,
+      },
+    }),
+  );
+}
+
+export default validate(config);
