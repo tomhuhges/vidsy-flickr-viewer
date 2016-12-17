2bb569e67a83ecf77f5a3f88e904b1a127233f7d
tomhuhges <merzbear@gmail.com>
2016-12-17 09:20:52 +0000

    make Images a stateless function

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-class Images extends React.Component {
-  render() {
-    return (
-      <div className="images">
-        {this.props.images.map(image =>
-          <img key={image.id} src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} alt={image.title} />,
-        )}
-      </div>
-    );
-  }
-}
+const Images = ({ images }) =>
+  (
+    <div className="images">
+      {images.map(image =>
+        <img key={image.id} src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} alt={image.title} />,
+      )}
+    </div>
+  );
 
 Images.propTypes = {
   images: React.PropTypes.array.isRequired,
