030b8741790d24b8dcdbc4323026d0eb5e3ec7a1
tomhuhges <merzbear@gmail.com>
2016-12-19 04:13:08 +0000

    add Gallery tests

--- src/components/Gallery.test.jsx
+++ src/components/Gallery.test.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Gallery from './Gallery';
 
-test('Gallery component renders the text of the Gallery', () => {
+test('Gallery renders a div with className of `img`', () => {
+  const images = [{
+    farm: 1,
+    id: 1,
+    secret: 1,
+    server: 1,
+  }];
+  const wrapper = shallow(
+    <Gallery images={images} />,
+  );
+  const div = wrapper.find('.img');
+  expect(div.is('div')).toBe(true);
+});
+
+test('Gallery renders one div for each object of the images array', () => {
+  const images = [{
+    farm: 1,
+    id: 1,
+    secret: 1,
+    server: 1,
+  },
+  {
+    farm: 1,
+    id: 1,
+    secret: 1,
+    server: 1,
+  }];
+  const wrapper = shallow(
+    <Gallery images={images} />,
+  );
+  expect(wrapper.find('div')).length === 2;
 });
